02843272946891df10a78680619b80f74fd2e03f
heineiuo <heineiuo@gmail.com>
2016-03-16 20:46:40 +0800

    fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var webpack = require('webpack-stream')
 var uglify = require("gulp-uglify")
 var cssmin = require('gulp-cssmin')
 var sass = require('gulp-sass')
-var filter = require('gulp-filter')
 var htmlmin = require('gulp-htmlmin')
 var concat = require('gulp-concat')
 var jst = require('gulp-jst3')
@@ -22,7 +21,7 @@ gulp.task('client-example-style', function () {
   return gulp.src('client-example/style/index.scss')
     .pipe(sass())
     .pipe(cssmin())
-    .pipe(gulp.dest('dist/client/www/css'))
+    .pipe(gulp.dest('dist/client/example/css'))
 })
 
 gulp.task('client-example-js', function () {
@@ -35,19 +34,18 @@ gulp.task('client-example-js', function () {
       }
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/client/www/js'))
+    .pipe(gulp.dest('dist/client/example/js'))
 })
 
 gulp.task('client-example-img', function () {
   return gulp.src('client-example/images/**/*')
-    .pipe(gulp.dest('dist/client/www/images'))
+    .pipe(gulp.dest('dist/client/example/images'))
 })
 
 gulp.task('client-example-html', function () {
   return gulp.src('client-example/index.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(reveasy({base: process.cwd()+'/dist'}))
-    .pipe(gulp.dest('dist/client/www'))
+    .pipe(gulp.dest('dist/client/example'))
 })
 
 
@@ -60,11 +58,11 @@ gulp.task('client-example-template', function () {
       ignorePath: process.cwd()+'/client-example/template'
     }))
     .pipe(concat('template.js'))
-    .pipe(gulp.dest('dist/client/www/js/'))
+    .pipe(gulp.dest('dist/client/example/js/'))
 })
 
 gulp.task('client-example-template-concat', ['client-example-template'], function () {
-  return gulp.src('dist/client/www/js/template.js')
+  return gulp.src('dist/client/example/js/template.js')
     .pipe(webpack({
       cache: false,
       target: 'web',
@@ -75,7 +73,7 @@ gulp.task('client-example-template-concat', ['client-example-template'], functio
       }
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/client/www/js'))
+    .pipe(gulp.dest('dist/client/example/js'))
 })
 
 gulp.task('client-example', [
@@ -86,7 +84,7 @@ gulp.task('client-example', [
   'client-example-template-concat',
   'client-example-img'], function () {
   return gulp.src('client-example/images/**/*')
-    .pipe(gulp.dest('dist/client/www/images'))
+    .pipe(gulp.dest('dist/client/example/images'))
 })
 
 gulp.task('client-libs', function () {
